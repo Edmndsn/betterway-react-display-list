055a0cfb198edfc4b44a92ab1ecfeb07a639ff6b
Edmndsn <edmondson.jackw@gmail.com>
2022-06-15 20:21:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { namesList } from "./data.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {namesList.map(item => {
+        return (
+          <ul>
+            <li key={item}>{item},</li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
---
+++ src/data.js
@@ -0,0 +1,1 @@
+export const namesList = ['John', 'David', 'Jack', 'James', 'Jim']
